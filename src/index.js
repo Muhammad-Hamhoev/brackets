a930abe63b7b06276aa83e3ed32afd81d4032f20
Maga <ded.hamhoev@gmail.com>
2021-03-15 19:02:36 +0300

    task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, brackets) {
+
+  var obj = {};
+  var arr = [];
+  
+  for(var j = 0; j < brackets.length; j += 1) {
+    obj[brackets[j][0]] = brackets[j][1];
+  }
+
+  arr.push(str.charAt(0));
+
+  for(var i = 1; i < str.length; i += 1) {
+    if(str.charAt(i) === obj[arr[arr.length - 1]]) {
+      arr.pop();
+    } else {
+      arr.push(str.charAt(i)); 
+    }
+  }
+
+  if (arr.length > 0) { 
+    return false;
+  }
+  return true;
+};
